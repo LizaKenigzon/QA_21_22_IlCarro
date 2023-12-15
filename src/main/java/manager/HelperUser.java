d765423786879b8dc0e954a492f3fd30992b30f8
LizaKenigzon <elizaveta.murashkina@bk.ru>
2023-12-15 18:43:55 +0200

    loginSuccessTestPassed

--- src/main/java/manager/HelperUser.java
+++ src/main/java/manager/HelperUser.java
@@ -1,9 +1,28 @@
 package manager;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class HelperUser extends HelperBase{
     public HelperUser(WebDriver wd) {
         super(wd);
     }
+    public void openLoginRegistrationForm(){
+        click(By.cssSelector("a[ng-reflect-router-link='login']"));
+    }
+    public void fillLoginRegistrationForm(String email, String password){
+        type(By.xpath("//input[@id='email']"),email);
+        type(By.xpath("//input[@id='password']"),password);
+    }
+    public void submitLogin(){
+        click(By.xpath("//button[@type='submit']"));
+    }
+    public void clickOkAfterLoginSuccess(){
+        click(By.xpath("//button[@class='positive-button ng-star-inserted']"));
+    }
+    public boolean isLogged(){
+        return
+                isElementPresent(By.xpath("//a[@href='logout']"));
+    }
+
 }
--- src/test/java/tests/LoginTests.java
+++ src/test/java/tests/LoginTests.java
@@ -1,11 +1,17 @@
 package tests;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class LoginTests extends TestBase{
 
     @Test
-    public void test(){
+    public void loginSuccess(){
+        app.getHelperUser().openLoginRegistrationForm();
+        app.getHelperUser().fillLoginRegistrationForm("elizaveta.murashkina@bk.ru", "Aa12345$");
+        app.getHelperUser().submitLogin();
+        app.getHelperUser().clickOkAfterLoginSuccess();
 
+        Assert.assertTrue(app.getHelperUser().isLogged());
     }
 }
