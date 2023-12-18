e34c2966885f595bbb064abe1d2722bc2b6ec831
LizaKenigzon <elizaveta.murashkina@bk.ru>
2023-12-18 19:06:41 +0200

    pre&postConditions

--- src/main/java/manager/HelperUser.java
+++ src/main/java/manager/HelperUser.java
@@ -3,26 +3,39 @@ package manager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
-public class HelperUser extends HelperBase{
+public class HelperUser extends HelperBase {
     public HelperUser(WebDriver wd) {
         super(wd);
     }
-    public void openLoginForm(){
+
+    public void openLoginForm() {
         click(By.cssSelector("a[ng-reflect-router-link='login']"));
     }
-    public void fillLoginForm(String email, String password){
-        type(By.xpath("//input[@id='email']"),email);
-        type(By.xpath("//input[@id='password']"),password);
-    }
-    public void submitLogin(){
-        click(By.xpath("//button[@type='submit']"));
-    }
-    public void clickOkAfterLoginSuccess(){
-        click(By.xpath("//button[@class='positive-button ng-star-inserted']"));
-    }
-    public boolean isLogged(){
-        return
-                isElementPresent(By.xpath("//a[@href='logout']"));
+
+    public void fillLoginForm(String email, String password) {
+        type(By.xpath("//input[@id='email']"), email);
+        type(By.xpath("//input[@id='password']"), password);
     }
 
+    public void submitLogin() {
+        click(By.xpath("//button[@type='submit']"));
+    }
+
+    public void clickOkAfterLoginSuccess() {
+        click(By.xpath("//button[@class='positive-button ng-star-inserted']"));
+    }
+
+    public boolean isLogged() {
+        return
+                isElementPresent(By.xpath("//a[text()=' Logout ']"));
+    }
+
+    public void logout() {
+        click(By.xpath("//a[text()=' Logout ']"));
+    }
+
+    public boolean isLoggedWithOkWindow() {
+        return
+                isElementPresent(By.xpath("//button[@class='positive-button ng-star-inserted']"));
+    }
 }
--- src/test/java/tests/LoginTests.java
+++ src/test/java/tests/LoginTests.java
@@ -1,18 +1,45 @@
 package tests;
 
+import manager.HelperBase;
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class LoginTests extends TestBase{
+
+    @BeforeMethod
+    public void preConditions(){
+            if(app.getHelperUser().isLogged()){
+                app.getHelperUser().logout();
+            }
+        }
 
     @Test
     public void loginSuccess(){
         app.getHelperUser().openLoginForm();
         app.getHelperUser().fillLoginForm("elizaveta.murashkina@bk.ru", "Aa12345$");
         app.getHelperUser().submitLogin();
-        app.getHelperUser().clickOkAfterLoginSuccess();
 
         //Assert.assertTrue(app.getHelperUser().isLogged());
         Assert.assertEquals(app.getHelperUser().getMessage(),"Logged in success");
+
+    }
+
+    @Test
+    public void loginSuccessModel(){
+        app.getHelperUser().openLoginForm();
+        app.getHelperUser().fillLoginForm("elizaveta.murashkina@bk.ru", "Aa12345$");
+        app.getHelperUser().submitLogin();
+
+        Assert.assertTrue(app.getHelperUser().isLogged());
+        //Assert.assertEquals(app.getHelperUser().getMessage(),"Logged in success");
+    }
+
+    @AfterMethod
+    public void postConditions(){
+        if(app.getHelperUser().isLoggedWithOkWindow()){
+            app.getHelperUser().clickOkAfterLoginSuccess();
+        }
     }
 }
